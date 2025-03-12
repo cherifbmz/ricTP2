89b75b14e9e58347379e4dbfe6ad5c44a969844e
boumehraz ahmed cherif <boumehrazcherif@gmail.com>
2025-03-12 11:35:07 +0100

    cost function and gradients

--- ric.py
+++ ric.py
@@ -10,3 +10,10 @@ def model(X,W,b):
     A=1/(1+np.exp(-Z))
     return A
 
+def function_cout(A,y):
+    return 1/len(y)*np.sum(-y*np.log(A)-(1-y)*np.log(1-A))
+
+def gradients(A,X,y):
+    dW=1/len(y)*np.dot(X.T,A-y)
+    db=1/len(y)*np.sum(A-y)
+    return(dW,db)
