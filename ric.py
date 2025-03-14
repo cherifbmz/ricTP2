d43e8c36c48bad6b158e0b01467b244db25c6a73
boumehraz ahmed cherif <boumehrazcherif@gmail.com>
2025-03-14 14:35:44 +0100

    implement back propagation et update_parameters method

--- ric.py
+++ ric.py
@@ -9,7 +9,7 @@ def initialize(dimentions):
 
     return parameters
 
-def segmoid(Z):
+def sigmoid(Z):
         return 1/(1+np.exp(-Z))
 
 
@@ -19,19 +19,38 @@ def forward_propagation(X,paramaters):
 
     for c in range(1,C+1):
       Z=paramaters["W"+str(c)].dot(activation["A"+str(c-1)])+paramaters["b"+str(c)]
-      activation["A"+str(c)]=segmoid(Z)
+      activation["A"+str(c)]=sigmoid(Z)
 
     return activation
 
-def function_cout(A,y):
-    return 1/len(y)*np.sum(-y*np.log(A)-(1-y)*np.log(1-A))
-
-def gradients(A,X,y):
-    dW=1/len(y)*np.dot(X.T,A-y)
-    db=1/len(y)*np.sum(A-y)
-    return(dW,db)
 
 
+def function_cout(A, y):
+    m = y.shape[1]  
+    return (-1/m) * np.sum(y * np.log(A) + (1 - y) * np.log(1 - A))
+
+def back_propagation(y,activations,paramaters):
+    m=y.shape[1]
+    C=len(paramaters)//2
+
+    dZ=activations['A'+str(C)]-y
+    gradients={}
+    for c in reversed((range(1,C+1))):
+        gradients["dW"+str(c)]=1/m*np.dot(dZ,activations["A"+str(c-1)].T)
+        gradients["db"+str(c)]=1/m*np.sum(dZ,axis=1,keepdims=True )
+        if c>1:
+            dZ=np.dot(paramaters["W"+str(c)].T,dZ)*activations["A"+str(c-1)]*(1-activations["A"+str(c-1)])
+   
 
 
- 
+    return gradients
+
+def update_parameters(gradients,parameters,learning_rate):
+    C=len(parameters)//2
+     
+    for c in range(1,C+1):
+        parameters["W"+str(c)]=parameters["W"+str(c)]-learning_rate*gradients["dW"+str(c)]
+        parameters["b"+str(c)]=parameters["b"+str(c)]-learning_rate*gradients["db"+str(c)] 
+
+    return parameters
+
